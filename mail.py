25ce8f172e53e454e0cd78b7a997ec0606b5cdf2
koreanluke <minjae@letscoding.kr>
2022-07-16 14:35:47 +0900

    연산자

--- mail.py
+++ mail.py
@@ -1,1 +1,14 @@
-print("hello")
+from gtts import gTTS
+from playsound import playsound
+import os
+
+text = '顏㣷㗿贳凹'
+tts = gTTS(text = text , lang = 'ko')
+#tts.save("./tovoice.mp3")
+
+filename = "tovoice"
+fileExtension = "mp3"
+fillpath:str = fr"{os. getcwd()}{filename}.{fileExtension}".replace("\\","/")
+
+tts.save(fillpath)
+playsound(fillpath)
---
+++ op.py
@@ -0,0 +1,16 @@
+#연산자 = operator
+
+a=4
+b=3
+print(a+b)
+print(a-b)
+
+a=10
+b=5
+print(a*b)
+
+a=9
+b=4
+print(a/b)
+print(a//b)
+print(a%b)
